066530b542c6a238ac5a930781edacba0fd279af
Adam BAISAGOUROV <abaisago@e2r2p20.42.fr>
2018-07-09 19:30:32 +0200

    j04

---
+++ j04/ex07/ft_find_next_prime.c
@@ -0,0 +1,45 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: abaisago <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/07/09 16:46:43 by abaisago          #+#    #+#             */
+/*   Updated: 2018/07/09 17:53:11 by abaisago         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+int		ft_is_prime(int nb)
+{
+	int		i;
+
+	if (nb == 0 || nb == 1)
+		return (0);
+	if (nb == 1 || nb == 2)
+		return (1);
+	if (nb % 2 == 0 || nb % 3 == 0)
+		return (0);
+	i = 5;
+	while (i * i <= nb)
+	{
+		if (nb % i == 0)
+		{
+			return (0);
+		}
+		i += 2;
+	}
+	return (1);
+}
+
+int		ft_find_next_prime(int nb)
+{
+	int		res;
+
+	res = 1;
+	while (!ft_is_prime(nb))
+	{
+		nb += 1;
+	}
+	return (nb);
+}
