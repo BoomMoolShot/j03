e52e2a976ab552196142a60dc44b45f93ac73d76
Adam BAISAGOUROV <abaisago@e3r7p19.42.fr>
2018-07-10 10:39:19 +0200

    recursive power changed to work with 0

--- j04/ex01/ft_recursive_factorial.c
+++ j04/ex01/ft_recursive_factorial.c
@@ -6,17 +6,17 @@
 /*   By: abaisago <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/08 20:34:17 by abaisago          #+#    #+#             */
-/*   Updated: 2018/07/10 09:57:45 by abaisago         ###   ########.fr       */
+/*   Updated: 2018/07/10 10:38:24 by abaisago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_recursive_factorial(int nb)
 {
-	if (nb > 1 && nb < 13)
+	if (nb > 0 && nb < 13)
 	{
 		return (nb * ft_recursive_factorial(nb - 1));
 	}
-	if (nb == 1)
+	if (nb == 0)
 	{
 		return (1);
 	}
