34e72c03e3525126c441aadea5cf1b3936d54959
Adam BAISAGOUROV <abaisago@e3r7p19.42.fr>
2018-07-10 10:53:49 +0200

    fixed fobonacci overflow

--- j04/ex04/ft_fibonacci.c
+++ j04/ex04/ft_fibonacci.c
@@ -6,12 +6,16 @@
 /*   By: abaisago <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 00:34:34 by abaisago          #+#    #+#             */
-/*   Updated: 2018/07/09 12:56:26 by abaisago         ###   ########.fr       */
+/*   Updated: 2018/07/10 10:52:00 by abaisago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_fibonacci(int index)
 {
+	if (index > 46)
+	{
+		return (0);
+	}
 	if (index == 0 || index == 1)
 		return (index);
 	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
