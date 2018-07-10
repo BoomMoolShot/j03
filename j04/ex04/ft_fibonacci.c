7e2dd2cfd4960246d989468bfe13fbc5b897cc43
Adam BAISAGOUROV <abaisago@e3r7p19.42.fr>
2018-07-10 11:05:13 +0200

    fibonacci fix negative numbers

--- j04/ex04/ft_fibonacci.c
+++ j04/ex04/ft_fibonacci.c
@@ -6,12 +6,16 @@
 /*   By: abaisago <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/09 00:34:34 by abaisago          #+#    #+#             */
-/*   Updated: 2018/07/10 10:52:00 by abaisago         ###   ########.fr       */
+/*   Updated: 2018/07/10 11:04:27 by abaisago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int		ft_fibonacci(int index)
 {
+	if (index < 0)
+	{
+		return (-1);
+	}
 	if (index > 46)
 	{
 		return (0);
