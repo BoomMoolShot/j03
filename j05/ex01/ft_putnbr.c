2236a81f79d72205f2367c1dd9de98f2f76bcc73
Adam BAISAGOUROV <abaisago@e3r7p18.42.fr>
2018-07-14 09:26:29 +0200

    mbr lol

--- j05/ex01/ft_putnbr.c
+++ j05/ex01/ft_putnbr.c
@@ -6,7 +6,7 @@
 /*   By: abaisago <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/05 12:02:16 by abaisago          #+#    #+#             */
-/*   Updated: 2018/07/06 15:16:23 by abaisago         ###   ########.fr       */
+/*   Updated: 2018/07/13 10:14:15 by abaisago         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,15 +14,10 @@ void	ft_putchar(char c);
 
 void	convert_to_positive(long int *n)
 {
-	*n *= -1;
-}
-
-void	print_negative(long int *n)
-{
 	if (*n < 0)
 	{
 		ft_putchar('-');
-		convert_to_positive(n);
+		*n *= -1;
 	}
 }
 
@@ -31,8 +26,8 @@ void	ft_putnbr(int nb)
 	long int n;
 
 	n = nb;
-	print_negative(&n);
-	if (n > 10)
+	convert_to_positive(&n);
+	if (n >= 10)
 	{
 		ft_putnbr(n / 10);
 		ft_putnbr(n % 10);
